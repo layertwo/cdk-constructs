b3da6a55440e98ab238686d626431ffe032f4198
Lucas Messenger <lcmessen@amazon.com>
2026-01-11 16:09:58 -0800

    chore: project created with projen

---
+++ .projenrc.ts
@@ -0,0 +1,17 @@
+import { awscdk } from 'projen';
+const project = new awscdk.AwsCdkConstructLibrary({
+  author: 'Lucas Messenger',
+  authorAddress: '1335960+layertwo@users.noreply.github.com',
+  cdkVersion: '2.200.0',
+  defaultReleaseBranch: 'mainline',
+  jsiiVersion: '~5.9.0',
+  name: 'layertwo-cdk-constructs',
+  projenrcTs: true,
+  repositoryUrl: 'https://github.com/lcmessen/cdk-constructs',
+
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],             /* Build dependencies for this module. */
+  // packageName: undefined,  /* The "name" in package.json. */
+});
+project.synth();
